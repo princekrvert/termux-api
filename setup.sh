d998ae1183ebafd4384ced291f16e9a77239a214
PRINCE KUMAR <56459297+princekrvert@users.noreply.github.com>
2022-03-08 05:09:24 +0530

    Update setup.sh
    Setup bin error

--- setup.sh
+++ setup.sh
@@ -9,7 +9,11 @@ pkg isntall clang
 # now compile the api 
 clang api.c -o api 
 # move file to bin to use as the cmd 
-mv api /usr/bin 
+mv api ~
+cd 
+mv api ..
+cd ..
+mv api usr/bin
 echo -e "\e[32;1m Read the github page to understand the cmd uses"
 echo -e "\e[35;1m https://github.com/princekrvert/termux-api"
 
